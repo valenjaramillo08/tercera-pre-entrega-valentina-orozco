956d3d7cd61b9c00ed653945cc602b0f0965222e
Valentina Orozco <valenjaramillo08@gmail.com>
2023-05-16 20:23:57 -0500

    Entrega lista

--- familia/AppCoder/views.py
+++ familia/AppCoder/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from AppCoder.models import familia
+
 
 # Create your views here.
+def templates(request):
+    contexto = {
+        "familiares": familia.objects.all()
+    }
+    http_responde = render(
+        request=request,
+        template_name= 'familia/index.html',
+        context=contexto
+    )
+    return http_responde
--- familia/familia/urls.py
+++ familia/familia/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from AppCoder.views import templates
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('templates/', templates)
 ]
